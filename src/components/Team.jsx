a4d20748af475befdc76b01514f5ffa80aa330af
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-12-17 17:44:25 +0530

    Create team cards

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,30 +3,34 @@ import React from "react";
 export const Team = (props) => {
   return (
     <div id="team" className="text-center">
-      <div className="container">
-        <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Meet the Team</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
-        </div>
-        <div id="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
-                  </div>
+      <div>
+        <h2>Meet the Team</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+          dapibus leonec.
+        </p>
+      </div>
+      <div className="home-container">
+        {props.data ? (
+          props.data.map((d, i) => (
+            <div key={`${d.name}-${i}`} className="profile-card">
+              <div className="img">
+                <img src={d.img || "./man.jpg"} alt={d.name} />
+              </div>
+              <div className="caption">
+                <h3>{d.name}</h3>
+                <p>{d.job}</p>
+                <div className="social-links">
+                  <i className="fa-brands fa-facebook"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                  <i className="fa-brands fa-instagram"></i>
                 </div>
-              ))
-            : "loading"}
-        </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );

7ece6ac250eee252d090e3459a8b6ef89e5f4433
root2j <rutujd1111@gmail.com>
2024-12-18 18:24:05 +0530

    Added background color to the From the desk cards

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -7,9 +7,11 @@ export const Features = (props) => {
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>FROM THE DESK</h2>
         </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
+        <div className="row" style={{padding: "0 15px"}}>
+          {/* First group */}
+          <div className="col-md-12 feature-group elm">
+            <div className="row">
+              {props.data && props.data.slice(0, 2).map((d, i) => (
                 <div 
                   key={`${d.title}-${i}`} 
                   className="col-xs-12 col-md-6"
@@ -18,8 +20,24 @@ export const Features = (props) => {
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                 </div>
-              ))
-            : "Loading..."}
+              ))}
+            </div>
+          </div>
+          {/* Second group */}
+          <div className="col-md-12 feature-group elm">
+            <div className="row">
+              {props.data && props.data.slice(2, 4).map((d, i) => (
+                <div 
+                  key={`${d.title}-${i}`} 
+                  className="col-xs-12 col-md-6"
+                >
+                  <img src={d.icon} alt={d.title} className="img-fluid"/>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>

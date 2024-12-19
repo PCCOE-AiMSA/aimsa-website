f29940a73104d7ed209b8479e1d490498cc7058b
Atharvakamtalwar <atharvakamtalwar@gmail.com>
2024-12-20 00:46:58 +0530

    Added sliding header and font updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-export const Header = (props) => {
+export const Header = () => {
+  const slides = [
+    { id: 1, src: "img/intro-bg.jpg", alt: "intro1" },
+    { id: 2, src: "img/intro-bg.jpg", alt: "intro2" },
+    { id: 3, src: "img/intro-bg.jpg", alt: "intro3" },
+    { id: 4, src: "img/intro-bg.jpg", alt: "intro4" },
+    { id: 5, src: "img/intro-bg.jpg", alt: "intro5" },
+    { id: 6, src: "img/intro-bg.jpg", alt: "intro6" },
+    { id: 7, src: "img/intro-bg.jpg", alt: "intro7" },
+  ];
+
   return (
-    <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-              
-              </div>
+    <div className="header">
+      <div className="slider">
+        <div className="slide-track">
+          {/* Original slides */}
+          {slides.map((slide) => (
+            <div className="slide" key={slide.id}>
+              <img src={slide.src} alt={slide.alt} />
             </div>
-          </div>
+          ))}
+          {/* Duplicate slides for smooth infinite scroll */}
+          {slides.map((slide) => (
+            <div className="slide" key={`duplicate-${slide.id}`}>
+              <img src={slide.src} alt={slide.alt} />
+            </div>
+          ))}
         </div>
       </div>
-    </header>
+       {/* Add Text and Tagline below carousel */}
+       <div className="text-container">
+          <h1 className="main-heading">AIMSA</h1>
+          <p className="tagline">Innovating the Future of AI and Technology</p>
+        </div>
+    </div>
   );
 };

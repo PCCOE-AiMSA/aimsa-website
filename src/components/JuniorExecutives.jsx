c6b9097c6d50e98d1d9ef5739fc8d1235ff87639
Ketan Agrawal <agrawal11ketan@gmail.com>
2024-12-21 17:29:43 +0530

    Updated junior executives component

--- src/components/JuniorExecutives.jsx
+++ src/components/JuniorExecutives.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, {useState } from "react";
 
 const ProfileImage = ({ src, alt }) => {
   const [hasError, setHasError] = useState(false);
